accb4212f1fe2be34dce6c6be7feee52c41ae1b4
loaki07 <loakesh.i@gmail.com>
2022-03-18 14:17:30 +0530

    add: dockerfile for dot_marketplace_node Phase2_Milestone1

--- mongoDb/setup.sh
+++
@@ -1,6 +0,0 @@
-mongo <<EOF
-use admin;
-db.auth('root', 'dot_marketplace');
-use authentication-service;
-db.createUser({user: 'marketplace_admin', pwd: 'marketplace_pwd', roles: [{role: 'readWrite', db: 'authentication-service'}]});
-EOF
